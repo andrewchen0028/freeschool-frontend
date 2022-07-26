8d877701fa07c9d1d5196c7f6c63adcf3f81e20f
Andrew Chen <andrewchen0028@gmail.com>
2022-07-25 20:02:00 -0700

    implement focus

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import axios from 'axios'
 function App() {
   const url = "http://localhost:3001/api/"
   const [graph, setGraph] = useState({ nodes: [], links: [] })
+  const [focus, setFocus] = useState(null)
 
   const refresh = () =>
     axios.get(url + 'graph/').then((graph) => { setGraph(graph.data) })
 
   useEffect(() => { refresh() }, [])
-  useEffect(() => { renderGraph(graph, () => { }) }, [graph])
+  useEffect(() => { renderGraph(graph, setFocus) }, [graph])
 
   return (
     <div className="App">
-      <button onClick={() => refresh()}>Refresh</button>
+      <button onClick={() => refresh()}>refresh graph</button>
+      {focus == null ? <h1>click a node to focus</h1> :
+        <div>
+          <h1>focused on node {focus.id}</h1>
+          <button onClick={() => setFocus(null)}>exit focus</button>
+        </div>}
       <div id='graph' />
     </div>
   );
