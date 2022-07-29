ab9b851ba9106dba38d8229eebc243bc54483ba0
Andrew Chen <andrewchen0028@gmail.com>
2022-07-28 19:43:31 -0700

    formatting

--- src/App.js
+++ src/App.js
@@ -6,55 +6,43 @@ import Node from "./components/Node"
 import url from "./globals"
 import AddNodeForm from "./components/AddNodeForm"
 
-// TODO: implement node title form validation (no duplicates)
-// todo: remove staged title after submit
 // TODO: implement prerequisite selection by node title search
 
 function App() {
   const [graph, setGraph] = useState({ nodes: [], links: [] })
   const [focus, setFocus] = useState(null)
 
-  const refresh = () => {
-    axios
-      .get(url + "/graph")
-      .then((response) => {
-        setGraph(response.data)
-        console.log("refresh():", response)
-      })
-  }
+  const refresh = () => axios
+    .get(url + "/graph")
+    .then((response) => {
+      setGraph(response.data)
+      console.log("refresh():", response)
+    })
 
-  const addNode = (stagedNodeTitle) => {
-    axios
-      .post(url + "/graph/nodes", {
-        sourceId: focus.id,
-        title: stagedNodeTitle
-      })
-      .then((response) => {
-        refresh()
-        setFocus(response.data)
-        console.log("addNode():", response)
-      })
-  }
+  const addNode = (stagedNodeTitle) => axios
+    .post(url + "/graph/nodes", {
+      sourceId: focus.id,
+      title: stagedNodeTitle
+    })
+    .then((response) => {
+      refresh()
+      setFocus(response.data)
+      console.log("addNode():", response)
+    })
 
-  const deleteNode = (node) => {
-    axios
-      .delete(url + "/graph/nodes/" + node.id)
-      .then((response) => {
-        refresh()
-        setFocus(null)
-        console.log("deleteNode():", response)
-      })
-  }
 
-  useEffect(() => {
-    refresh()
-    console.log("useEffect() []")
-  }, [])
+  const deleteNode = (node) => axios
+    .delete(url + "/graph/nodes/" + node.id)
+    .then((response) => {
+      refresh()
+      setFocus(null)
+      console.log("deleteNode():", response)
+    })
 
-  useEffect(() => {
-    renderGraph(graph, setFocus)
-    console.log("useEffect() [graph]")
-  }, [graph])
+
+  useEffect(() => { refresh() }, [])
+
+  useEffect(() => { renderGraph(graph, setFocus) }, [graph])
 
   return (
     <div className="App">
